44b1cd40a541bc09f84d808e9d5e5ffcdeb6ba52
PeZunino <pedrozsantos@hotmail.com>
2025-03-26 23:59:42 -0300

    feat(auth): create and config JWT auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controller/authenticate.controller';
 import { CreateUserController } from './controller/create-user.controller';
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
@@ -9,10 +11,12 @@ import { PrismaService } from './prisma/prisma.service';
 		ConfigModule.forRoot({
 			validate: env => envSchema.parse(env),
 			isGlobal:true
-		})
+		}),
+		AuthModule
 	],
 	controllers: [
-		CreateUserController
+		CreateUserController,
+		AuthenticateController
 	],
 	providers: [
 		PrismaService
---
+++ src/controller/authenticate.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Post, UnauthorizedException, UsePipes } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { z } from 'zod';
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe';
+import { PrismaService } from '@/prisma/prisma.service';
+
+
+const authenticateUserBodySchema = z.object({
+	cpf: z.string()
+		.length(11),
+	password: z.string(),
+});
+
+type AuthenticateUserBodySchema = z.infer<typeof authenticateUserBodySchema>
+
+@Controller('/sessions')
+export class AuthenticateController{
+
+	constructor(
+		private prisma: PrismaService,
+		private jwt: JwtService
+	){}
+
+	@Post()
+	@UsePipes(new ZodValidationPipe(authenticateUserBodySchema))
+	async handle(@Body() body:AuthenticateUserBodySchema){
+		const {
+			cpf,password
+		} = body;
+
+		const foundUser = await this.prisma.user.findUnique({where:{cpf}});
+
+		if(!foundUser){
+			throw new UnauthorizedException('Wrong e-mail password combination');
+		}
+
+		const isPasswordValid = compare(password,foundUser.password);
+
+		if(!isPasswordValid){
+			throw new UnauthorizedException('Wrong e-mail password combination');
+		}
+
+		const token = this.jwt.sign({
+			sub: foundUser.id,
+			role: foundUser.role
+		});
+
+		return{access_token:token};
+	}
+}
