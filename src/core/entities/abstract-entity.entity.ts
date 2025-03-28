4f882dbdf715e5e3f97ee86946cbec64c5ea3492
PeZunino <pedrozsantos@hotmail.com>
2025-03-27 23:00:47 -0300

    fix(entity): id getter

--- src/core/entities/abstract-entity.entity.ts
+++ src/core/entities/abstract-entity.entity.ts
@@ -3,12 +3,16 @@ import { UniqueEntityID } from './unique-id-entity';
 export abstract class Entity<Props>{
 	private _id:UniqueEntityID;
 
-	protected constructor(protected props:Props, private id?:UniqueEntityID){
+	protected constructor(protected props:Props, id?:UniqueEntityID){
 		this.props = props;
 
 		this._id = id ?? new UniqueEntityID();
 	}
 
+	get id() {
+		return this._id;
+	}
+	
 	public equals(entity: Entity<any>) {
 		if (entity === this) {
 			return true;
